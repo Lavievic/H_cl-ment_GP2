8c31ce272a0102b10b647117d5b54f958496b321
Lavieville <clement.lavieville@hotmail.fr>
2019-11-22 16:55:13 +0100

    mur

--- TP1 15nov(copie du 8nov)/app 8nov/app 8nov/app 8nov.cpp
+++ TP1 15nov(copie du 8nov)/app 8nov/app 8nov/app 8nov.cpp
@@ -1,6 +1,9 @@
 // app 8nov.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
 
+// app 8nov.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
 #include "pch.h"
 #include <iostream>
 #include <SFML/Graphics.hpp>
@@ -11,12 +14,13 @@
 #include <SFML/Graphics.hpp>
 #include "Entity.h"
 #include "Ball.h"
+#include "Wall.h"
 #include <Box2D/Box2D.h>
 
 using namespace sf;
-
 static std::vector<Entity> CharList;
 static std::vector<Ball> BallList;
+static std::vector<Wall> WallList;
 sf::Color hsv(int hue, float sat, float val)
 {
 	hue %= 360;
@@ -47,7 +51,9 @@ sf::Color hsv(int hue, float sat, float val)
 	}
 }
 static Vector2f shPos(0, 0);
-
+static int height;
+static int width;
+//static Vector2f ballpos(shPos);
 
 int squareSpeed = 3;
 int BallSpeed = 5;
@@ -58,33 +64,65 @@ sf::FloatRect boundingBox;
 Vector2f Alpha;
 sf::FloatRect otherBox;
 
-bool collision = false;
-bool collisionR = false;
-bool collisionL = false;
-bool collisionU = false;
-bool collisionD = false;
 Vector2f Beta;
 
 void world(sf::RenderWindow &win)
 {
+
+	for (int i = 0; i < WallList.size(); i++)
+	{
+		if (CharList[0].tank.getGlobalBounds().intersects(WallList[i].wallet.getGlobalBounds()))
+		{
+			CharList[0].position.x = Beta.x;
+			CharList[0].position.y = Beta.y;
+			squareSpeed = 0;
+		}
+	}
 	if (CharList[0].tank.getGlobalBounds().intersects(CharList[1].tank.getGlobalBounds()))
 	{
 		CharList[0].position.x = Beta.x;
 		CharList[0].position.y = Beta.y;
 		squareSpeed = 0;
-		
 	}
 	else
 	{
-		
 		Beta.x = CharList[0].position.x;
 		Beta.y = CharList[0].position.y;
 		squareSpeed = 3;
 	}
+	for (int i = 0; i < BallList.size(); i++)
+	{
+		if (WallList[0].wallet.getGlobalBounds().intersects(BallList[i].ball.getGlobalBounds()))
+		{
+			BallList[i].r = -BallList[i].r;
+			int A = 0;
+		}
+		if (WallList[1].wallet.getGlobalBounds().intersects(BallList[i].ball.getGlobalBounds()))
+		{
+			BallList[i].r = -BallList[i].r;
+			int A = 0;
+		}
+		if (WallList[2].wallet.getGlobalBounds().intersects(BallList[i].ball.getGlobalBounds()))
+		{
+			BallList[i].u = -BallList[i].u;
+			int A = 0;
+		}
+		if (WallList[3].wallet.getGlobalBounds().intersects(BallList[i].ball.getGlobalBounds()))
+		{
+			BallList[i].u = -BallList[i].u;
+			int A = 0;
+		}
+	}
 
 
-
-
+}
+void drawWallet(sf::RenderWindow &win)
+{
+	for (Wall &Elem : WallList)
+	{
+		win.draw(Elem.wallet);
+		Elem.SetPosition();
+	}
 }
 void drawTank(sf::RenderWindow &win)
 {
@@ -108,16 +146,16 @@ void drawBall(sf::RenderWindow &win)
 	{
 
 		win.draw(Elem.ball);
-		Elem.ball.move(Elem.u / 5, Elem.r / 5);
-		
+		Elem.ball.move(Elem.u / 8, Elem.r / 8);
+
 	}
 }
 
 int main()
 {
 	bool Shoot = false;
-	Entity Player = Entity(Vector2f(200, 200), Vector2f(80, 80));
-	Entity Ennemy = Entity(Vector2f(1700, 900), Vector2f(80, 80));
+	Entity Player = Entity(Vector2f(500, 500), Vector2f(80, 80));
+	Entity Ennemy = Entity(Vector2f(80, 80), Vector2f(30, 30));
 
 
 	CharList.push_back(Player);
@@ -127,9 +165,17 @@ int main()
 	settings.antialiasingLevel = 2;
 	static RectangleShape sh;
 
-
-
 	sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!", sf::Style::Default, settings);
+	height = window.getSize().y;
+	width = window.getSize().x;
+	Wall Up = Wall(Vector2f(0, 0), Vector2f(window.getSize().x, 3));
+	Wall Down = Wall(Vector2f(0, (window.getSize().y) - 3), Vector2f(window.getSize().x, 3));
+	Wall Left = Wall(Vector2f(0, 0), Vector2f(3, height));
+	Wall Right = Wall(Vector2f(window.getSize().x - 3, 0), Vector2f(3, height));
+	WallList.push_back(Up);
+	WallList.push_back(Down);
+	WallList.push_back(Left);
+	WallList.push_back(Right);
 #pragma region MyRegion
 
 
@@ -138,9 +184,9 @@ int main()
 	window.setVerticalSyncEnabled(true);
 
 #pragma endregion
-	while (window.isOpen())
+	while (window.isOpen())//on passe tout le temps DEBUT DE LA FRAME 
 	{
-		sf::Event event;
+		sf::Event event;//recup les evenement clavier/pad
 		while (window.pollEvent(event))
 		{
 			switch (event.type) {
@@ -203,11 +249,12 @@ int main()
 		{
 
 			float u = sf::Joystick::getAxisPosition(0, sf::Joystick::U);
-			float v = sf::Joystick::getAxisPosition(0, sf::Joystick::V);
-			if (u > 25 || u < -25 || v > 25 || v < -25)
+			float r = sf::Joystick::getAxisPosition(0, sf::Joystick::V);
+			if (u > 25 || u < -25 || r>25 || r < -25)
 			{
-				float angle = (atan2f(u, v) * 180) / 3.1415926535;
+				float angle = (atan2(u, r) * 180) / 3.141592654;
 				CharList[0].Viseur.setRotation(-angle);
+
 
 				if (!Shoot && sf::Joystick::isButtonPressed(0, 5))
 				{
@@ -225,11 +272,12 @@ int main()
 					Shoot = false;
 				}
 			}
+
 		}
 
 
 		window.clear();
-
+		drawWallet(window);
 		drawTank(window);
 		drawViseur(window);
 		drawBall(window);
